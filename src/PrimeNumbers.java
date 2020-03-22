214ceeb4f99fed4c579208efcdb3b4cbee202d59
Paweł Rubach <pawel.rubach@gmail.com>
2020-03-23 00:58:51 +0100

    Initial repo for HW1

---
+++ src/PrimeNumbers.java
@@ -0,0 +1,12 @@
+public class PrimeNumbers {
+
+    public static void primes(int n) {
+        // Your code here
+        System.out.println("2, 3, 5, 7");
+    }
+
+
+    public static void main(String[] args) {
+        primes(4);
+    }
+}
